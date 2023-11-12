5f321ca1a1df54b685e94d8e8fca3c5741e29409
paulhth <paul.horvath@student.upt.ro>
2023-11-12 14:24:26 +0200

    alu corrected

--- project/project/src/ALU.vhd
+++ project/project/src/ALU.vhd
@@ -1,12 +1,12 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL; -- Add this line
+use IEEE.STD_LOGIC_1164.ALL;	 --has std_logic and std_logic_vector
+use IEEE.NUMERIC_STD.ALL;		 --has SIGNED() and UNSIGNED() functions
 
 entity ALU is
     Port (
         A : in STD_LOGIC_VECTOR(3 downto 0);
         B : in STD_LOGIC_VECTOR(3 downto 0);
-        op : in STD_LOGIC_VECTOR(1 downto 0);
+        op : STD_LOGIC;
         Y : out STD_LOGIC_VECTOR(3 downto 0)
     );
 end ALU;
@@ -15,14 +15,13 @@ architecture Behavioral of ALU is
 begin
     process(A, B, op)
     begin
-        case op is
-            when "00" => -- Assume op "00" is addition
-                Y <= STD_LOGIC_VECTOR(SIGNED(A) + SIGNED(B)); -- Use SIGNED for signed arithmetic
-            when "01" => -- Assume op "01" is subtraction
-                Y <= STD_LOGIC_VECTOR(SIGNED(A) - SIGNED(B)); -- Use SIGNED for signed arithmetic
-            -- Add more operations as needed
-            when others =>
-                Y <= (others => '0'); -- Default case
-        end case;
+			if op = '1' then
+				Y<= STD_LOGIC_VECTOR(SIGNED(A) - SIGNED(B)); 
+			else 
+				Y<= STD_LOGIC_VECTOR(SIGNED(A) + SIGNED(B));
+        end if;
     end process;
 end Behavioral;
+
+
+-- SIGNED(A) tells VHDL that A is a vector of bits representing a signed number
